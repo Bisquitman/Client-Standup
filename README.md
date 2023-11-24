5a57376a067e325c3535f038c86ff716a6c2680b
Bisquitman <lynx8309@gmail.com>
2023-11-24 23:24:35 +0300

    получение данных с сервера и селекты

---
+++ README.md
@@ -0,0 +1,69 @@
+# Client-Standup
+
+Клиентская часть для сервиса регистрации на Standup (FullStack Интенсив от школы MethED - Сервис записи на StandUp (21.11.2023 — 25.11.2023))
+
+<!--Блок информации о репозитории в бейджах-->
+
+![Static Badge](https://img.shields.io/badge/Bisquitman-Client-Standup)
+![GitHub top language](https://img.shields.io/github/languages/top/Bisquitman/Client-Standup)
+![GitHub](https://img.shields.io/github/license/Bisquitman/Client-Standup)
+![GitHub Repo stars](https://img.shields.io/github/stars/Bisquitman/Client-Standup)
+![GitHub issues](https://img.shields.io/github/issues/Bisquitman/Client-Standup)
+
+<!--Установка-->
+
+## Установка
+
+У вас должны быть установлены [зависимости проекта](https://github.com/Bisquitman/Client-Standup#зависимости)
+
+1. Клонирование репозитория
+
+   `git clone https://github.com/Bisquitman/Client-Standup.git`
+
+2. Переход в директорию Client-Standup
+
+   `cd Client-Standup`
+
+3. Установка [зависимостей](https://github.com/Bisquitman/Client-Standup#зависимости)
+
+   `npm i`
+
+4. Запуск сервера
+
+   4.1. Build-режим
+
+   `npm run build`
+
+   4.2. Dev-режим
+
+   `npm run dev`
+
+---
+
+<!--зависимости-->
+## Зависимости
+(версии указаны на момент разработки)
+
+- "devDependencies":
+  - "@vitejs/plugin-legacy": "^5.2.0",
+  - "autoprefixer": "^10.4.16",
+  - "postcss": "^8.4.31",
+  - "postcss-sort-media-queries": "^5.2.0",
+  - "sass": "^1.69.5",
+  - "vite": "^5.0.2"
+
+- "dependencies":  
+  - "normalize.css": "^8.0.1",
+  - "tom-select": "^2.3.1"
+
+***
+
+## Полезные ссылки
+
+### Frontend
+
+- [Макет figma](https://www.figma.com/file/S8tWzyFV28xy3RCHdgu0qV/Stand-Up-(Maraphone)?type=design&node-id=0%3A1&mode=design&t=bf9VlSqwtsi8RbBO-1)
+- [Vite](https://vitejs.dev/)
+- [Normalize](https://necolas.github.io/normalize.css/)
+- [Бесплатные шрифты](https://gwfh.mranftl.com/fonts)
+- [https://tom-select.js.org/](https://tom-select.js.org/) - плагин для кастомизации select
--- main.js
+++ main.js
@@ -1,1 +1,135 @@
-import './style.scss';
+import "./style.scss";
+import TomSelect from "tom-select";
+import { scrollIntoView } from "seamless-scroll-polyfill";
+
+// const API_URL = "http://192.168.1.2:8080";
+const API_URL = "https://full-lively-animal.glitch.me";
+const MAX_COMEDIANS = 6;
+
+const bookingComediansList = document.querySelector(".booking__comedians-list");
+const bookingSection = document.getElementById("booking");
+const btnReserve = document.querySelector(".event__button_reserve");
+
+const declOfNum = (n, titles) =>
+  titles[n % 10 === 1 && n % 100 !== 11 ? 0 : n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20) ? 1 : 2];
+
+const createComedianBlock = (comedians) => {
+  const bookingComedian = document.createElement("li");
+  bookingComedian.className = "booking__comedian";
+
+  const bookingSelectComedian = document.createElement("select");
+  bookingSelectComedian.className = "booking__select booking__select_comedian";
+  bookingSelectComedian.id = "select-comedian";
+
+  const bookingSelectTime = document.createElement("select");
+  bookingSelectTime.className = "booking__select booking__select_time";
+
+  const inputHidden = document.createElement("input");
+  inputHidden.type = "hidden";
+  inputHidden.name = "booking";
+
+  const bookingHallBtn = document.createElement("button");
+  bookingHallBtn.className = "booking__hall-btn";
+  bookingHallBtn.type = "button";
+
+  bookingComedian.append(bookingSelectComedian, bookingSelectTime, inputHidden);
+
+  const bookingTomSelectComedian = new TomSelect(bookingSelectComedian, {
+    create: false,
+    hideSelected: true,
+    placeholder: "Выбрать комика",
+    sortField: {
+      field: "text",
+      direction: "asc",
+    },
+    options: comedians.map((comedian) => ({
+      value: comedian.id,
+      text: comedian.comedian,
+    })),
+  });
+  const bookingTomSelectTime = new TomSelect(bookingSelectTime, {
+    create: false,
+    hideSelected: true,
+    placeholder: "Время",
+    sortField: {
+      field: "text",
+      direction: "asc",
+    },
+  });
+  bookingTomSelectTime.disable();
+
+  bookingTomSelectComedian.on("change", (id) => {
+    bookingTomSelectComedian.blur();
+    bookingTomSelectTime.enable();
+    const { performances } = comedians.find((comedian) => comedian.id === id);
+    bookingTomSelectTime.clear();
+    bookingTomSelectTime.clearOptions();
+    bookingTomSelectTime.addOptions(
+      performances.map((performance) => ({
+        value: performance.time,
+        text: performance.time,
+      })),
+    );
+    bookingHallBtn.remove();
+  });
+
+  bookingTomSelectTime.on("change", (time) => {
+    if (!time) return;
+
+    const idComedian = bookingTomSelectComedian.getValue();
+    const { performances } = comedians.find((item) => item.id === idComedian);
+    const { hall } = performances.find((item) => item.time === time);
+    inputHidden.value = `${idComedian},${time}`;
+    bookingTomSelectTime.blur();
+    bookingHallBtn.innerHTML = `
+      ${hall}
+      <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path fill-rule="evenodd" clip-rule="evenodd"
+          d="M9 3.5H3V9.5H5V5.5H9V3.5ZM3 21.5V15.5H5V19.5H9V21.5H3ZM15 3.5V5.5H19V9.5H21V3.5H15ZM19 15.5H21V21.5H15V19.5H19V15.5ZM7 7.5H11V11.5H7V7.5ZM7 13.5H11V17.5H7V13.5ZM17 7.5H13V11.5H17V7.5ZM13 13.5H17V17.5H13V13.5Z"
+          fill="currentColor" />
+      </svg>
+    `;
+    bookingComedian.append(bookingHallBtn);
+  });
+
+  const createNextBookingComedian = () => {
+    if (bookingComediansList.children.length <= MAX_COMEDIANS) {
+      const nextComediansBlock = createComedianBlock(comedians);
+      bookingComediansList.append(nextComediansBlock);
+    }
+    bookingTomSelectTime.off("change", createNextBookingComedian);
+  };
+
+  bookingTomSelectTime.on("change", createNextBookingComedian);
+
+  return bookingComedian;
+};
+
+const getComedians = async () => {
+  const response = await fetch(`${API_URL}/comedians`);
+
+  return response.json();
+};
+
+const init = async () => {
+  btnReserve.addEventListener("click", (e) => {
+    e.preventDefault();
+    scrollIntoView(bookingSection, { behavior: "smooth", block: "start", inline: "start" });
+  });
+  
+  const countComedians = document.querySelector(".event__info-item_comedians");
+
+  countComedians.innerHTML = `<span class="event__info-number">0</span>`;
+  const comedians = await getComedians();
+  console.log("comedians: ", comedians);
+  countComedians.innerHTML = `<span class="event__info-number">${comedians.length}</span>${declOfNum(comedians.length, [
+    "комик",
+    "комика",
+    "комиков",
+  ])}`;
+
+  const comedianBlock = createComedianBlock(comedians);
+
+  bookingComediansList.append(comedianBlock);
+};
+init();
