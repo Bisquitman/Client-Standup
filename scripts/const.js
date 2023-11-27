e6d50aef21591fd9ca20ca8d6e5aecfa60afd09c
Bisquitman <lynx8309@gmail.com>
2023-11-27 20:40:35 +0300

    change API_URL to glitch

--- scripts/const.js
+++ scripts/const.js
@@ -1,3 +1,3 @@
-export const API_URL = "http://localhost:8080";
-// export const API_URL = "https://full-lively-animal.glitch.me";
+// export const API_URL = "http://localhost:8080";
+export const API_URL = "https://full-lively-animal.glitch.me";
 export const MAX_COMEDIANS = 6;
